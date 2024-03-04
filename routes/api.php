bf1695f6b3a225e3fd13beb7dde73e70807d86af
vasunaik1999 <vasunaik1999@gmail.com>
2024-03-04 13:29:22 +0530

    first commit

---
+++ routes/api.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('products', [ProductController::class, 'index']);
+Route::get('products/{product}', [ProductController::class, 'show']);
+Route::post('products', [ProductController::class, 'store']);
+
+Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+    return $request->user();
+});
